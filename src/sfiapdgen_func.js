81ef354b8c358e41b69b09c5b26654f369f1b5a1
testudoq <stephenhstewart@gmail.com>
2024-04-28 08:58:20 +1200

    removed lor call also not used

--- src/sfiapdgen_func.js
+++ src/sfiapdgen_func.js
@@ -759,30 +759,7 @@ function searchForText() {
     } catch (error) {
         console.error("An error occurred:", error.message);
     }
-}
-
-/**
- * Updates the URL with the selected Levels of Responsibility (LoR) checkboxes.
- * 
- * This function retrieves all LoR checkboxes on the page, filters them to only include
- * the checked ones, retrieves their 'id' attribute, and joins them with '+' as a separator.
- * The resulting string is then set as the URL hash.
- */
-function updateURLWithLorCheckboxes() {
-    // Retrieve all LoR checkboxes on the page.
-    const lorCheckboxes = document.querySelectorAll('input[type=checkbox][id^="lor-"]');
-  
-    // Filter the checkboxes to only include the checked ones.
-    const selectedLorCheckboxes = Array.from(lorCheckboxes)
-      .filter(checkbox => checkbox.checked)
-      // Retrieve the 'id' attribute of each checked LoR checkbox.
-      .map(checkbox => checkbox.id);
-  
-    // Join the selected LoR checkboxes with '+' as a separator and set it as the URL hash.
-    const urlHash = selectedLorCheckboxes.join('+');
-    window.location.hash = urlHash;
-  }
-  
+}  
 
 /**
  * Retrieves the value of a cookie by its name.
